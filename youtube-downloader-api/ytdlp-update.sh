e05dae92bae6b1716a2003cabefbac6a04ba7d1e
aitubesmart-creator <aitubesmart@gmail.com>
2025-09-04 18:25:30 +0800

    Delete youtube-downloader-api directory

--- youtube-downloader-api/ytdlp-update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Installing/Updating yt-dlp..."
-# Download the latest yt-dlp binary to the project's root directory
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
-# Make it executable
-chmod a+rx ./yt-dlp
-echo "yt-dlp is ready."
